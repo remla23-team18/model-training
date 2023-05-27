3a17ee6f6c7d59c3999d1b9acc2770cec2ac2772
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2023-05-27 16:48:09 +0200

    Fix linting issues

--- src/model_training/preprocess.py
+++ src/model_training/preprocess.py
@@ -1,17 +1,34 @@
-import nltk
-nltk.download('stopwords')
+"""Reusable preprocessing script for cleaning restaurant review text data."""
 import re
+
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+
+nltk.download("stopwords")
+
 ps = PorterStemmer()
-all_stopwords = stopwords.words('english')
-all_stopwords.remove('not')
+all_stopwords = stopwords.words("english")
+all_stopwords.remove("not")
+
 
 def clean_review(review):
-    review = re.sub('[^a-zA-Z]', ' ', review)
+    """Clean review text.
+
+    Parameters
+    ----------
+    review : str
+        Review text to be cleaned.
+
+    Returns
+    -------
+    review : str
+        Cleaned review text.
+    """
+    review = re.sub("[^a-zA-Z]", " ", review)
     review = review.lower()
     review = review.split()
-    review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
-    review = ' '.join(review)
+    review = [ps.stem(word) for word in review if word not in set(all_stopwords)]
+    review = " ".join(review)
     return review
--- src/model_training/train.py
+++ src/model_training/train.py
@@ -1,39 +1,43 @@
-import numpy as np
-import pandas as pd
-import re
-
-from sklearn.feature_extraction.text import CountVectorizer
-import pickle
-import joblib
+"""Training pipeline for the sentiment analysis model."""
 import os
+import pickle
+
+import joblib
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+
 from src.model_training.preprocess import clean_review
 
-dataset = pd.read_csv('a1_RestaurantReviews_HistoricDump.tsv', delimiter = '\t', quoting = 3)
 
-corpus=[]
+dataset = pd.read_csv(
+    "a1_RestaurantReviews_HistoricDump.tsv", delimiter="\t", quoting=3
+)
+
+corpus = []
 for i in range(0, 900):
-  review = clean_review(dataset['Review'][i])
-  corpus.append(review)
+    review = clean_review(dataset["Review"][i])
+    corpus.append(review)
 
-cv = CountVectorizer(max_features = 1420)
+cv = CountVectorizer(max_features=1420)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, -1].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.20, random_state=0
+)
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
 
 # Create the train folder if it doesn't exist
-if not os.path.exists('models'):
-    os.makedirs('models')
+if not os.path.exists("models"):
+    os.makedirs("models")
 # Saving BoW dictionary to later use in prediction
-pickle.dump(cv, open('models/c1_BoW_Sentiment_Model.pkl', "wb"))
+pickle.dump(cv, open("models/c1_BoW_Sentiment_Model.pkl", "wb"))
 # Saving the classifier model to later use in prediction
-joblib.dump(classifier, 'models/c2_Classifier_Sentiment_Model') 
-
+joblib.dump(classifier, "models/c2_Classifier_Sentiment_Model")
 
 
 # Model performance measure
