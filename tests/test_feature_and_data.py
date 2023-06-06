bb0535e2a106a74e10134f61048d86cf14596feb
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2023-06-06 12:19:13 +0200

    Begin to fix linting and formatting issues

--- tests/test_feature_and_data.py
+++ tests/test_feature_and_data.py
@@ -1,6 +1,7 @@
 """Tests to check if the data is cleaned properly."""
 from model_training.preprocess import clean_review
 
+
 def test_clean_review() -> None:
     """It returns a cleaned review."""
     review = "This is a test."
